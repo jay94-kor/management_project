b7172e2e3913cc8cf5d714db831b0d81371e6f97
jay94-kor <148678404+jay94-kor@users.noreply.github.com>
2024-07-12 13:18:34 +0900

    z

--- src/components/auth.py
+++
@@ -1,3 +0,0 @@
-def authenticate_user(username, password):
-    # 인증 로직을 추가합니다.
-    return username == "admin" and password == "password"
--- src/components/common.py
+++
@@ -1,3 +0,0 @@
-def validate_data(data):
-    # 데이터 유효성 검사 로직을 추가합니다.
-    return True
